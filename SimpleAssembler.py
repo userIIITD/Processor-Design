cc67d3ba2e2080287a7b2fcfaaf0f81f84ea45e8
Hariom <hackerdc8287@gmail.com>
2025-02-10 10:22:51 +0530

    Update SimpleAssembler.py

--- SimpleAssembler.py
+++ SimpleAssembler.py
@@ -1,1 +1,32 @@
-#This is the main file
+class RV32i:
+    def __init__(self,R,I,S,B,J,U,file);
+        self.R=R
+        self.I=I
+        self.S=S
+        self.B=B
+        self.J=J
+        self.U=U
+        self.file=file
+    def R(self,R,file):
+        Ri=self.R
+
+
+    def I(self,I,file):
+        Ii=self.I
+
+    def S(self,S,file):
+        Si=self.S
+
+    def B(self,B,file):
+        Bi=self.B
+
+    def J(self,J,file):
+        Ji=self.J
+
+    def U(self,U,file):
+        Ui=self.U
+
+Inst=RV32i()
+
+
+        
