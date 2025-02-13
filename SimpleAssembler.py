29301c650f17684263569dfea1ceb34b3acd0a37
HackIIITD1234 <chaitanya24158@iiitd.ac.in>
2025-02-13 13:19:38 +0530

    type of instruction added

--- SimpleAssembler.py
+++ SimpleAssembler.py
@@ -4,6 +4,8 @@ file = open('')
 
 Register= {"zero": "00000", "ra":"00001", "sp": "00010", "gp": "00011", "tp": "00100", "t0": "00101", "t1": "00110", "t2": "00111", "s0": "01000", "fp": "01000", "s1": "01001", "a0": "01010", "a1": "01011", "a2": "01100", "a3": "01101", "a4": "01110", "a5": "01111", "a6": "10000", "a7": "10001", "s2": "10010", "s3": "10011", "s4": "10100", "s5": "10101", "s6": "10110", "s7": "10111", "s8": "11000", "s9": "11001", "s10": "11010", "s11": "11011", "t3": "11100", "t4": "11101", "t5": "11110", "t6": "11111"}
 
+type_of_inst = {"add" : "R", "sub" : "R", "slt" : "R", "srl" : "R", "or" : "R", "and" : "R", "addi" : "I", "lw" : "R", "jalr" : "I", "sw" : "S", "beq" : "B", "blt" : "B", "bne" : "B", "jal" : "J"}
+
 def sext(num, bit = 12):
     return format(num & (2**bit - 1), f"0{bit}b")
 
@@ -19,7 +21,7 @@ def R(instruction, f):
 
 def I(instruction, f):
     f3={"lw":["010","0010011"],"addi":["000","0010011"],"jalr":["000","1100111"]} #[f3,opcode]
-    for i in I:
+    for i in instruction:
         name=i.split()[0]
         imm=sext(int(i.split()[1].split(",")[1].split("(")[0]),12)
         rd=Register[i.split()[1].split(",")[0]]
@@ -44,11 +46,7 @@ def B(instruction, f):
         imm = sext(int(i.split()[1].split(",")[2]), 12)
         rs1 = Register[i.split()[1].split(",")[0]]
         rs2 = Register[i.split()[1].split(",")[1]]
-
         f.write(f"{imm[:7]}{rs2}{rs1}{func3[name]}{imm[7:]}{opcode}")
 
 def J(instruction, f):
-    pass
-
-def U(instruction, f):
     pass
